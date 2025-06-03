0dc05d8e02e80827615a864a8514df737dd7fc58
nishthasingh20 <singhnishtha206@gmail.com>
2025-06-04 02:27:13 +0530

    testing stuff

--- App.js
+++ App.js
@@ -1,22 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Dimensions, SafeAreaView } from 'react-native';
 
 export default function App() {
-  console.log("app executed");
-
-
+  console.log(Dimensions.get("screen"));
+  
   return (
-    <View style={styles.container}>
-      <Text>Hello React Native</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style ={{
+        backgroundColor: "lightblue",
+        width: "50%",
+        height: 70,
+      }}>
+
+      </View>
+
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'beige',
     alignItems: 'center',
     justifyContent: 'center',
   },
